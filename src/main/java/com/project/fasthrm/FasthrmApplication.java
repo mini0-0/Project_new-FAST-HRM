1f7f24f3f1afa4d4b974dedd3f920e5a23799b04
mini0-0 <nalala8200@naver.com>
2025-03-12 22:14:58 +0900

    # 1 spring boot 시작
    spring boot 초기 세팅

---
+++ src/main/java/com/project/fasthrm/FasthrmApplication.java
@@ -0,0 +1,13 @@
+package com.project.fasthrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasthrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasthrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fasthrm/FasthrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fasthrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasthrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
