6b5fe1a30479eee530ebe6d4ea2af598e7b82269
mini0-0 <nalala8200@naver.com>
2025-04-22 23:40:46 +0900

    # 3 - MasterMain에 대한 service 구현
    - MasterMainService 인터페이스 및 구현체(MasterMainServiceImpl) 생성 및 추상화 구조 설계

---
+++ src/main/java/com/project/fasthrm/service/impl/MasterMainServiceImpl.java
@@ -0,0 +1,29 @@
+package com.project.fasthrm.service.impl;
+
+import com.project.fasthrm.dto.response.MasterMainDto;
+
+import com.project.fasthrm.repository.MasterMainQueryRepository;
+import com.project.fasthrm.service.MasterMainService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MasterMainServiceImpl implements MasterMainService {
+
+    private final MasterMainQueryRepository repository;
+
+    @Override
+    public MasterMainDto getMasterMainDashboard(Long placeId) {
+        return MasterMainDto.builder()
+                .eduRevenueList(repository.getEduRevenue(placeId))
+                .monthlyEduRevenueList(repository.getMonthlyEduRevenue(placeId))
+                .monthlyRegistrationList(repository.getMonthlyRegistration(placeId))
+                .build();
+    }
+
+
+}
