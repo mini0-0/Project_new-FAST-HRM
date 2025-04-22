29390ee5fb0c6cdff62e479e32b919de62f11242
mini0-0 <nalala8200@naver.com>
2025-04-22 22:59:37 +0900

    # 3- JpaAuditingConfig 생성
    - CreatedAt, UpdatedAt을 자동으로 실행되게 구현

---
+++ src/main/java/com/project/fasthrm/config/JpaAuditingConfig.java
@@ -0,0 +1,18 @@
+package com.project.fasthrm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("system"); // 시스템 사용자로 설정
+    }
+}
