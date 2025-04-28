3157d8796932ee407b451b9c8ec7b7dfa69652db
mini0-0 <nalala8200@naver.com>
2025-04-29 00:03:21 +0900

    # 4 - update, delete 구현
    - WorkerManage에 update, delete 기능 구현

--- src/main/java/com/project/fasthrm/repository/query/WorkerQueryRepository.java
+++ src/main/java/com/project/fasthrm/repository/query/WorkerQueryRepository.java
@@ -1,11 +1,13 @@
 package com.project.fasthrm.repository.query;
 
-import com.project.fasthrm.dto.response.WorkerMainDto;
+import com.project.fasthrm.dto.response.WorkerManageDto;
 
 import java.util.List;
 
 public interface WorkerQueryRepository {
 
-    List<WorkerMainDto> findWorkerByPlaceId(Long placeId);
+    List<WorkerManageDto> findWorkerByPlaceId(Long placeId);
+    void updateWorkerManage(WorkerManageDto dto);
+    void deleteWorkerManage(Long userId);
 
 }
