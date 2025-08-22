4b2e6ef135217e2924f88cb322bfdd5ab9953fc3
mini0-0 <nalala8200@naver.com>
2025-08-23 00:38:12 +0900

    # 4 [수정] - dto 수정
    - Attendancestatus 변경에 따른 코드 수정
    - @Setter 추가로 인한 관련 코드 수정

--- src/main/java/com/project/fasthrm/dto/response/WorkerManageDto.java
+++ src/main/java/com/project/fasthrm/dto/response/WorkerManageDto.java
@@ -1,14 +1,17 @@
 package com.project.fasthrm.dto.response;
 
+import com.project.fasthrm.domain.type.AttendanceStatus;
 import com.querydsl.core.annotations.QueryProjection;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class WorkerManageDto {
 
@@ -25,8 +28,7 @@ public class WorkerManageDto {
     private LocalDateTime workStartTime; // 출근 시간
     private LocalDateTime workEndTime;   // 퇴근 시간
 
-    private Boolean todayAttendance; // 오늘 출근 여부
-    private Boolean isLate;          // 지각 여부
+    private AttendanceStatus todayAttendance; // 오늘 출근/지각/결석 여부
     private Boolean usedVacation;    // 휴가 사용 여부
     private Integer monthlyWorkHours; // 이번 달 누적 근무 시간
     private Integer weeklyWorkHours; // 이번 주 누적 근무 시간
@@ -37,7 +39,7 @@ public class WorkerManageDto {
     public WorkerManageDto(Long userId, Long placeId, String username, String password, String userRealName,
                            String userAddress, String userPhoneNumber, Integer workerSalary,
                            String scheduledWorkTime, LocalDateTime workStartTime, LocalDateTime workEndTime,
-                           Boolean todayAttendance, Boolean isLate, Boolean usedVacation,
+                           AttendanceStatus todayAttendance, Boolean usedVacation,
                            Integer monthlyWorkHours, Integer weeklyWorkHours) {
         this.userId = userId;
         this.placeId = placeId;
@@ -51,31 +53,10 @@ public class WorkerManageDto {
         this.workStartTime = workStartTime;
         this.workEndTime = workEndTime;
         this.todayAttendance = todayAttendance;
-        this.isLate = isLate;
         this.usedVacation = usedVacation;
         this.monthlyWorkHours = monthlyWorkHours;
         this.weeklyWorkHours = weeklyWorkHours;
 
-    }
-
-    public void setTodayAttendance(Boolean todayAttendance) {
-        this.todayAttendance = todayAttendance;
-    }
-
-    public void setIsLate(Boolean isLate) {
-        this.isLate = isLate;
-    }
-
-    public void setUsedVacation(Boolean usedVacation) {
-        this.usedVacation = usedVacation;
-    }
-
-    public void setMonthlyWorkHours(Integer monthlyWorkHours) {
-        this.monthlyWorkHours = monthlyWorkHours;
-    }
-
-    public void setWeeklyWorkHours(Integer weeklyWorkHours) {
-        this.weeklyWorkHours = weeklyWorkHours;
     }
 
 }
